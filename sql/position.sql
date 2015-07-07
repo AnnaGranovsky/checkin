556bbd5714b9c3aa3cc6e26f09187de2d94cb7c7
Maxim Uvarov <primevendetta@gmail.com>
2015-07-07 19:12:11 +0300

    Update position.sql

--- sql/position.sql
+++ sql/position.sql
@@ -28,3 +28,13 @@ INSERT INTO `position` (`id`, `u_id`, `longitude`, `latitude`, `accuracy`) VALUE
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+
+-- Дамп структуры для таблица checkin.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(50) NOT NULL DEFAULT '0',
+  `password` varchar(50) NOT NULL DEFAULT '0',
+  `mail` varchar(50) NOT NULL DEFAULT '0',
+  `admin` int(1) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
